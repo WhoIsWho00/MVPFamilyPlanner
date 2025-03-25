a7641e13eca28619c19536dfa974499b46415e7c
AndriiKr1 <shadowuals@gmail.com>
2025-03-25 11:41:13 +0100

    add swager,add field avatar and age fix bugs change version springboot!!!!!!!!!

---
+++ src/main/java/com/example/familyplanner/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.familyplanner.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI familyPlannerAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Family Planner API")
+                        .description("API for family planning application")
+                        .version("1.0"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
+    }
+}
