9bf9b4237110acf4b99034cd693b97c203d08537
AndriiKr1 <shadowuals@gmail.com>
2025-03-28 17:59:01 +0100

    fix SecC

--- src/main/java/com/example/familyplanner/Security/SecurityConfig.java
+++ src/main/java/com/example/familyplanner/Security/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173" ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
