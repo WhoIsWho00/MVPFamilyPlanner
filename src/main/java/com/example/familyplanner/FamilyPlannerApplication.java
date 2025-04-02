d213ffd4debcfd4d2d2605355c867bbd90d0ce97
John Doe <johndoe@example.com>
2025-04-03 00:21:49 +0200

    ACTUATOR added

--- src/main/java/com/example/familyplanner/FamilyPlannerApplication.java
+++ src/main/java/com/example/familyplanner/FamilyPlannerApplication.java
@@ -1,10 +1,10 @@
 package com.example.familyplanner;
 
-import com.example.familyplanner.Security.JWT.JwtCore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.example.familyplanner.Security.JWT.JwtCore;
 
 @SpringBootApplication
 //@EnableJpaRepositories("com.example.familyplanner.repository")
--- src/main/java/com/example/familyplanner/Security/JwtAuthFilter.java
+++ src/main/java/com/example/familyplanner/Security/JwtAuthFilter.java
@@ -1,21 +1,23 @@
 package com.example.familyplanner.Security;
 
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import com.example.familyplanner.Security.JWT.JwtCore;
 import com.example.familyplanner.service.UserService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
